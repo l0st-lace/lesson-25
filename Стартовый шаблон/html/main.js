b7fceab8ce4bc5b577e5c42ac7b1d8dd1279ccfc
Kirillba2011 <kirillba2011@gmail.com>
2026-04-05 14:02:07 +0300

    DZ

--- Стартовый шаблон/html/main.js
+++ Стартовый шаблон/html/main.js
@@ -1,1 +1,53 @@
+const iphone = document.querySelector(".iphone")
+const samsung = document.querySelector(".samsung")
 
+iphone.addEventListener('click', function (event) {
+    let clickTarget = event.target;
+    let activeButton = iphone.querySelector(".active");
+    let priceItem = iphone.querySelector(".price");
+    let currentPrice;
+    let activeButtonColor = iphone.querySelector(".active-border");
+
+    if (clickTarget.classList.contains("color-btn") && !clickTarget.classList.contains("active-border") ) {
+        clickTarget.classList.add("active-border");
+        activeButtonColor.classList.remove("active-border");
+    }
+
+    if (clickTarget.classList.contains("btn") && !clickTarget.classList.contains("active")) {
+        clickTarget.classList.add("active");
+        activeButton.classList.remove("active");
+        currentPrice = clickTarget.getAttribute("data-size");
+
+        if (currentPrice === "medium"){
+            priceItem.textContent = "Цена 80 000р"
+        }
+            if (currentPrice === "large"){
+        priceItem.textContent = "Цена 100 000р"
+    }
+    }
+})
+samsung.addEventListener('click', function (event) {
+    let clickTarget = event.target;
+    let activeButton = samsung.querySelector(".active");
+    let priceItem = samsung.querySelector(".price");
+    let currentPrice;
+    let activeButtonColor = samsung.querySelector(".active-border");
+
+    if (clickTarget.classList.contains("color-btn") && !clickTarget.classList.contains("active-border") ) {
+        clickTarget.classList.add("active-border");
+        activeButtonColor.classList.remove("active-border");
+    }
+
+    if (clickTarget.classList.contains("btn") && !clickTarget.classList.contains("active")) {
+        clickTarget.classList.add("active");
+        activeButton.classList.remove("active");
+        currentPrice = clickTarget.getAttribute("data-size");
+
+        if (currentPrice === "medium"){
+            priceItem.textContent = "Цена 80 000р"
+        }
+            if (currentPrice === "large"){
+        priceItem.textContent = "Цена 100 000 000р"
+    }
+    }
+})
